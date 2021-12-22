11d1685dee9e6e95cb051150884466fbc8197d98
J-S-G <81664984+J-S-G@users.noreply.github.com>
2021-12-21 23:28:06 -0700

    Add files via upload

---
+++ Assignment02.MutualExclusion/Assignment02.MutualExclusion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Assignment02.MutualExclusion
+{
+    class Program
+    {
+
+        static object _lock = new object();
+
+        static void Main()
+        {
+            Thread thread1 = new Thread(() => Write("+"));
+
+            Thread thread2 = new Thread(() => Write("-"));
+
+            thread1.Start();
+
+            thread2.Start();
+
+            thread1.Join();
+
+            thread2.Join();
+        }
+
+        static void Write(string message)
+        {
+            Monitor.Enter(_lock);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(message);
+            }
+
+            Monitor.Exit(_lock);
+        }
+    }
+}
---
+++ Assignment6.EFCore/Assignment6.EFCore/Migrations/20210723175422_DBCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Assignment6.EFCore.Migrations
+{
+    public partial class DBCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsComplete = table.Column<bool>(type: "bit", nullable: false),
+                    DateCompleted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
